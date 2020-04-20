91a3d45f2f3063248189e76c7765c18068104362
TylerNW3994 <TylerNW3994@gmail.com>
2020-04-20 15:44:14 -0400

    Revert "added other files"
    This reverts commit c6bb7c0557217496db6dbc621818b5aef605d1e1.

--- index.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-
-const token = "NzAxODQxODM0NDE4MjQxNjQ2.Xp3Xbw.RT1-22NLzY-cQ0MChjPST0eJ_GQ";
-
-bot.login(process.env.token);
