c6bb7c0557217496db6dbc621818b5aef605d1e1
Tyler Wallschleger <TylerNW3994@gmail.com>
2020-04-20 15:00:21 -0400

    added other files

---
+++ index.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const token = "NzAxODQxODM0NDE4MjQxNjQ2.Xp3Xbw.RT1-22NLzY-cQ0MChjPST0eJ_GQ";
+
+bot.login(process.env.token);
